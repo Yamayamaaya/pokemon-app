94a913aa47cb221cd3caa9f49f74543a7c845b6f
Yamayamaaya <100800509+Yamayamaaya@users.noreply.github.com>
2023-03-04 20:20:35 +0900

    1st_commit🎉

---
+++ src/components/card.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./card.css";
+const Card = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default}></img>
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardType">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="重さ">
+            重さ：
+            {pokemon.weight}
+          </p>
+        </div>
+        <div className="cardData">
+          <p className="高さ">
+            高さ：
+            {pokemon.height}
+          </p>
+        </div>
+        <div className="cardData">
+          <div className="能力">
+            <p>能力</p>
+            {pokemon.abilities.map((ability) => {
+              return <p>{ability.is_hidden || ability.ability.name}</p>;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
